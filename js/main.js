89a34b7ba7263b18c560e08e99c9f7919260f954
rlated1337 <rlated@protonmail.com>
2018-01-18 20:37:26 +0100

    implemented copying of url's

--- js/main.js
+++ js/main.js
@@ -220,30 +220,20 @@ app.controller('gifGenCtrl', function($scope, $http, $timeout, $rootScope, $filt
     });
   };
 
-  function copyToClipboard(val) {
-    var dummy = document.createElement("input");
-    document.body.appendChild(dummy);
-    $(dummy).css('display', 'none');
-    dummy.setAttribute("id", "dummy_id");
-    document.getElementById("dummy_id").value = val;
-    dummy.select();
-    document.execCommand("copy");
-    document.body.removeChild(dummy);
-  }
-
-
   $scope.shareContent = function()  {
-    //the data we actually want to copy
-    var copyData = $scope.links[$scope.dataIndex];
-    copyToClipboard(copyData);
-    console.log("copied");
+    copyToClipboard($scope.links[$scope.dataIndex]);
+    var dialog = bootbox.dialog({
+      message: '<p class="text-center">Copied URL ✅  Share it with your friends.</p>',
+      closeButton: false
+    });
+
+    window.setTimeout(function(){
+        bootbox.hideAll();
+    }, 2000); // 10 seconds expressed in milliseconds
   };
 
 
-
-
 });
-
 
 angular.module('gifGen')
   .filter('trustUrl', function($sce) {
@@ -251,3 +241,25 @@ angular.module('gifGen')
       return $sce.trustAsResourceUrl(url);
     };
   });
+
+function copyToClipboard(text) {
+  if (window.clipboardData && window.clipboardData.setData) {
+    // IE specific code path to prevent textarea being shown while dialog is visible.
+    return clipboardData.setData("Text", text);
+
+  } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+    var textarea = document.createElement("textarea");
+    textarea.textContent = text;
+    textarea.style.position = "fixed"; // Prevent scrolling to bottom of page in MS Edge.
+    document.body.appendChild(textarea);
+    textarea.select();
+    try {
+      return document.execCommand("copy"); // Security exception may be thrown by some browsers.
+    } catch (ex) {
+      console.warn("Copy to clipboard failed.", ex);
+      return false;
+    } finally {
+      document.body.removeChild(textarea);
+    }
+  }
+};
